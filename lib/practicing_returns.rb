943976b2510f8024d09d12b82d6eedae44fc2182
Priscilla Leyva <pleyva.co89@gmail.com>
2020-08-26 00:30:08 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,11 +1,12 @@
-
+require "pry"
 
 def hello(array)
   i = 0
-  collection = []
   while i < array.length
     collection << yield(array[i])
     i += 1
   end
-  collection
 end
+
+
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}"}
